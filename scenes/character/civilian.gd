7c964be80b1853ebe402eb300443f851ea71ef6d
binarydigitz01 <binarydigitz01@protonmail.com>
2024-12-11 21:13:38 +0530

    Added Builder

--- scenes/character/civilian.gd
+++ scenes/character/civilian.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-@onready var agent = $NavigationAgent2D
+#@onready var agent = $NavigationAgent2D
 func _ready() -> void:
 	$AnimatedSprite2D.play("walk")
 
 func _physics_process(delta: float) -> void:
-	agent.target_position = get_global_mouse_position()
-	var dir = to_local(agent.get_next_path_position()).normalized()
-	var new_velocity = dir*SPEED
-	agent.set_velocity_forced(new_velocity)
-	velocity = new_velocity
-	move_and_slide()
+	pass
+	#agent.target_position = get_global_mouse_position()
+	#var dir = to_local(agent.get_next_path_position()).normalized()
+	#var new_velocity = dir*SPEED
+	#agent.set_velocity_forced(new_velocity)
+	#velocity = new_velocity
+	#move_and_slide()
 	
--- scenes/water/water.gd
+++ scenes/water/water.gd
@@ -4,6 +4,5 @@ var travelled = []
 var quantity
 func _init(q, pos: Vector2i):
 	Global.water_array.append(self)
-	print("Group: ", get_groups())
 	quantity=q
 	travelled.append(pos)
