d06eb55069e522fbae6723249c34e41617171268
Egggggggggggg <infinitegamer090@gmail.com>
2024-12-11 23:59:10 +0530

    Pipes properly coded(64 lines of if else)

--- scenes/map/PipeLayer.gd
+++ scenes/map/PipeLayer.gd
@@ -6,11 +6,58 @@ func _on_water_timer_timeout() -> void:
 		water = water as Water
 		var current_coords = water.travelled[-1]
 		print("Water coords: ", current_coords)
-		var surrounding_cells = get_surrounding_cells(current_coords)
-		print("su", surrounding_cells)
-		for sc in surrounding_cells:
-			var atlas_coords = get_cell_atlas_coords(sc)
-			if atlas_coords.y == 0 and sc not in water.travelled:
-				water.travelled.append(sc)
+		var current_atlas = get_cell_atlas_coords(current_coords)
+		if current_atlas.y==2:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y+1))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y-1))
+			if (n1_cell.y == 2 or n1_cell.y == 13 or n1_cell.y == 12) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
 				print(water.travelled)
-				break
+			if (n2_cell.y == 2 or n1_cell.y == 14 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+		if current_atlas.y==8:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x+1,current_coords.y))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x-1,current_coords.y))
+			if (n1_cell.y == 8 or n1_cell.y == 13 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+			if (n2_cell.y == 8 or n2_cell.y == 12 or n2_cell.y == 14) and n2_cell not in water.travelled:
+				water.travelled.append(n2_cell)
+				print(water.travelled)
+		if current_atlas.y==12:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y-1))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x+1,current_coords.y))
+			if (n1_cell.y == 2 or n1_cell.y == 14 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+			if (n2_cell.y == 8 or n2_cell.y == 13 or n2_cell.y == 15) and n2_cell not in water.travelled:
+				water.travelled.append(n2_cell)
+				print(water.travelled)
+		if current_atlas.y==13:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y-1))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x+1,current_coords.y))
+			if (n1_cell.y == 2 or n1_cell.y == 14 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+			if (n2_cell.y == 8 or n1_cell.y == 13 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+		if current_atlas.y==14:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y+1))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x+1,current_coords.y))
+			if (n1_cell.y == 2 or n1_cell.y == 13 or n1_cell.y == 12) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+			if (n2_cell.y == 8 or n1_cell.y == 13 or n1_cell.y == 15) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+		if current_atlas.y==15:
+			var n1_cell=get_cell_atlas_coords(Vector2i(current_coords.x,current_coords.y+1))
+			var n2_cell=get_cell_atlas_coords(Vector2i(current_coords.x-1,current_coords.y))
+			if (n1_cell.y == 2 or n1_cell.y == 12 or n1_cell.y == 13) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
+			if (n2_cell.y == 8 or n1_cell.y == 12 or n1_cell.y == 14) and n1_cell not in water.travelled:
+				water.travelled.append(n1_cell)
+				print(water.travelled)
